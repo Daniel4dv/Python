f1e4cf3ff27fb8700cf0760b69e4bab795bcb239
Daniel4dv <141754367+Daniel4dv@users.noreply.github.com>
2024-07-03 09:16:04 -0300

    ok

--- python-udemy/secao03/conceitos_basicos/aula72(for).py
+++ python-udemy/secao03/conceitos_basicos/aula72(for).py
@@ -1,1 +1,13 @@
- 
+# texto =' python'
+
+# for letra in texto:
+#     print(letra)
+
+numeros = range(10) # determina uma vaiavel que possui um range especifico ex:(1->10) 1 até 9
+print(numeros)
+
+for numero in numeros:  #  0 -> 9
+    print(numero)
+
+    texto = iter('iteravel')
+    print(texto)
