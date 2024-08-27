40f618c2c0bf7a390400708228fb8c77edfec7ed
Daniel <141754367+Daniel4dv@users.noreply.github.com>
2024-08-27 16:25:34 -0300

    fim tuplas

--- curso-em-video/exercicios.py/exe076.py
+++ curso-em-video/exercicios.py/exe076.py
@@ -1,14 +1,14 @@
-tupla = ('produto1','R$75,99',\
-    'produto2','R$49,99',\
-    'produto3','R$89,99',\
-    'produto4','R$37,99')
+tupla = ('Capacete',75.99,\
+    'Meia',49.99,\
+    'Tenis',89,\
+    'Bracelete',370.99)
     
 i = int(0)
 print('-'*35)
-print("Tabela de preços:")
+print(f"{'TABELA DE PREÇOS':^35}")
 print('-'*35)
 for i in range(0,len(tupla),2):
-    print(f'{tupla[i]}....................{tupla[i+1]}')
+    print(f'{tupla[i]:.<30} R${tupla[i+1]:>7.2f}')
 
 
 # CONCLUIDO 
--- curso-em-video/exercicios.py/exe077.py
+++ curso-em-video/exercicios.py/exe077.py
@@ -1,4 +1,9 @@
 palavras = ('hoje', 'malandro', 'video','Mercado','computador','teclado')
-vogais = ('a','e','i','o','u')
 
-palavra[0] 
+
+for p in palavras:
+    print(f'\nNa palavra {p.upper()} temos:',end=' ')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra,end=' ')
+
