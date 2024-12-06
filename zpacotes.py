af49d172691884f07eba9d1a8fb93cf8121dbbbe
Daniel <141754367+Daniel4dv@users.noreply.github.com>
2024-12-06 09:43:00 -0300

    testes

--- zpacotes.py
+++ zpacotes.py
@@ -1,7 +1,11 @@
-# import pacotes_package.modulo
-from zpackage import fala_oi
+tupla = (1,5,7,34,65,8,9,5,3,65,7,555,6,8,9)
+lista = [2,8,4,7]
+# print(sorted(tupla))
+# print(tupla[-1])
 
-# from zpackage.modulob import fala_oi
+# print(tupla.index(65))
+lista.pop()
 
-fala_oi()
-
+lista.insert(1,3)
+lista.append(6)
+print(lista)
